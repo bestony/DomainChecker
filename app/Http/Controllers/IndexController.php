c85d6cde8f0aff33423211826303139dd6a0c1ae
Bestony <best.tony@foxmail.com>
2016-10-26 01:05:42 +0800

    引入模型

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Domain;
 
 class IndexController extends Controller
 {
    public function index(){
        return view('web.index');
+       
    }
 }
