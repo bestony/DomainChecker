9e9880a318109d09f3e69ef2050eeced47aa3a39
Bestony <best.tony@foxmail.com>
2016-10-26 02:40:28 +0800

    加入WHOIS查询功能更

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,23 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Log;
 use App\Http\Requests;
 use App\Domain;
 use Rainwsy\Aliyunmail\Send\Single;
 use Rainwsy\Aliyunmail\Auth;
 
+
 class IndexController extends Controller
 {
     public function index(){
+        
+        Log::info('Load IndexPage');
+        
         $dm= new Domain;
         $data = $dm->all();
         return view('web.Index',['data'=>$data]);
         
     }
     public function request(){
+        
+        Log::info('Load Request Page');
+        
         return view('web.Request');
     }
     public function save(Request $request){
+        
+        Log::info('Add New Domain:'.$request->domain);
+        
         $dm = new Domain;
         $dm->domain = $request->domain;
         $dm->status  = '未监测';
@@ -28,10 +39,26 @@ class IndexController extends Controller
         return view('web.Success');
     }
     public function check(){
-        $res = $this->SendMail('www.ixiqin.com');
-        var_dump($res);
+        $domain = new Domain;
+        $data=$domain->all();
+        foreach ($data as $dm){
+            if($this->getDomain($dm->domain) == '0'){
+                $update = \App\Domain::find($dm->id);
+                $update->status ='监测中,等待过期';
+                $update->save();
+            }else{
+                $this->SendMail($dm->domain);
+            }
+        }
+        echo 'Success';
+        
     }
+    // 发送邮件函数
+    // @arg $domain 域名
+    // @return restult 阿里云API接口返回值
     private function SendMail($domain){
+        
+        Log::info('Send Mail To Owner For Domain:'.$domain);
         
         $auth = Auth::config(getenv('DM_AK'), getenv('DM_SK'));
         $mail = new Single();
@@ -47,5 +74,18 @@ class IndexController extends Controller
         $send = $mail->send();
         
         return $send;
+    }
+    // 域名查询接口 
+    // @arg $domain
+    // @return boolean 
+    private function getDomain($domain){
+        
+        $domain = new \Phois\Whois\Whois($domain);
+        
+        if ($domain->isAvailable()) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
