ef8c1124ea7d6c68a83cb6b7671cd4169d5c8aee
Bestony <best.tony@foxmail.com>
2016-10-26 02:42:06 +0800

    加入添加域名提醒

--- resources/views/web/Request.blade.php
+++ resources/views/web/Request.blade.php
@@ -3,6 +3,7 @@
   <h1 class="content-subhead">新增域名</h1> 
   {!! Form::open(array('url'=>'/request','method'=>'POST','class'=>'pure-form')) !!}
   <fieldset class="pure-group">
+    <legend>需要输入根域名，如qq.com，输入二级域名可能会出现误报情况！</legend>
     <input class="pure-input-1" type="text" placeholder="填写您要监控的域名" name="domain">
   </fieldset>
   <button type="submit" class="pure-button pure-input-1-4 pure-button-primary">加入域名</button>
