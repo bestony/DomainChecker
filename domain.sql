bee3ece5f279018dfee432da836dd6602e355724
Bestony <best.tony@foxmail.com>
2016-10-26 08:32:29 +0800

    加入数据库结构，虚拟主机直接导入使用。

---
+++ domain.sql
@@ -0,0 +1,19 @@
+﻿# Host: localhost  (Version: 5.5.40)
+# Date: 2016-10-26 08:24:46
+# Generator: MySQL-Front 5.3  (Build 4.120)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "domain"
+#
+
+DROP TABLE IF EXISTS `domain`;
+CREATE TABLE `domain` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `domain` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
