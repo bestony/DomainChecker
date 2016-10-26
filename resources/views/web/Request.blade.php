ce27f8707b4158ce9e2ba79be20a7f2becb9e514
Bestony <best.tony@foxmail.com>
2016-10-26 08:46:15 +0800

    输入校验，为空不可用

--- resources/views/web/Request.blade.php
+++ resources/views/web/Request.blade.php
@@ -4,7 +4,7 @@
   {!! Form::open(array('url'=>'/request','method'=>'POST','class'=>'pure-form')) !!}
   <fieldset class="pure-group">
     <legend>需要输入根域名，如qq.com，输入二级域名可能会出现误报情况！</legend>
-    <input class="pure-input-1" type="text" placeholder="填写您要监控的域名" name="domain">
+    <input class="pure-input-1" type="text" placeholder="填写您要监控的域名" name="domain" required>
   </fieldset>
   <button type="submit" class="pure-button pure-input-1-4 pure-button-primary">加入域名</button>
 
